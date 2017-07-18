f7196bd99a34d07d79b31041f17888fbf1b10197
pravesh <choudharypravesh03@gmail.com>
2017-07-18 11:42:52 +0530

    node_modules removed from git remote

--- src/actions.js
+++ src/actions.js
@@ -3,19 +3,6 @@
  */
 
 export const SEARCH = 'SEARCH';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-
-/*
- * other constants
- */
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-};
-
 /*
  * action creators
  */
